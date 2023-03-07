bcf93ed974ef82005489868df3e6ed8d30c46385
ramandeep-softuvo <ramandeep.kaur@softuvo.in>
2023-03-07 11:38:38 +0530

    ios build issue resolved,Added ios app icon, Camera permissiom added for ios

--- lib/auth_screens/ui/first_screen.dart
+++ lib/auth_screens/ui/first_screen.dart
@@ -25,16 +25,16 @@ class FirstScreen extends StatelessWidget {
                   child: Image.asset(
                     firstLook,
                     width: SizeConfig.screenWidth / 1.2,
-                    height: SizeConfig.screenHeight / 2.4,
+                    height: SizeConfig.screenHeight / 2.4
                   ),
                 ),
                 SizedBox(
-                  height: SizeConfig.blockSizeVertical * 5,
+                  height: SizeConfig.blockSizeVertical * 5
                 ),
                 headingText(readyGetSetGo, SizeConfig.blockSizeHorizontal * 4.8,
                     colorBlack),
                 SizedBox(
-                  height: SizeConfig.blockSizeVertical * 2,
+                  height: SizeConfig.blockSizeVertical * 2
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 20, right: 20),
@@ -42,7 +42,7 @@ class FirstScreen extends StatelessWidget {
                       SizeConfig.blockSizeHorizontal * 3.8, colorGrey),
                 ),
                 SizedBox(
-                  height: SizeConfig.blockSizeVertical * 5,
+                  height: SizeConfig.blockSizeVertical * 5
                 ),
                 ElevatedButton(
                     onPressed: () {
@@ -51,9 +51,9 @@ class FirstScreen extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: colorRed,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                        borderRadius: BorderRadius.circular(8)
                       ),
-                      elevation: 15.0,
+                      elevation: 15.0
                     ),
                     child: SizedBox(
                         width: SizeConfig.screenWidth / 1.5,
